f5296d0e0bb8f455c7bff7071265288dd66b48ab
John W Kerns <jkerns@packetsar.com>
2026-03-22 09:48:09 -0700

    Updated with hop names

--- zabbix-net-mtr/mtr.sh
+++ zabbix-net-mtr/mtr.sh
@@ -1,4 +1,18 @@
 #!/bin/bash
 
 IP=$1
-mtr -r -c3 -w -b -p -j $IP
+mtr  -r -c3 -b -j $IP | jq '
+.report.hubs |= map(
+  if (.host | test("\\(")) then
+    . + {
+      name: (.host | capture("^(?<h>.+) \\((?<ip>.+)\\)$").h),
+      ip: (.host | capture("^(?<h>.+) \\((?<ip>.+)\\)$").ip)
+    }
+  else
+    . + {
+      name: "?",
+      ip: .host
+    }
+  end
+)
+'
